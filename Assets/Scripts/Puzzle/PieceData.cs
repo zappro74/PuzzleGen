ad34b8c69e14dfb5c01244cdcd152b4f278e0c3b
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-16 17:14:06 -0500

    added PieceDirection enum

--- Assets/Scripts/Puzzle/PieceData.cs
+++ Assets/Scripts/Puzzle/PieceData.cs
@@ -3,15 +3,31 @@ using UnityEngine;
 [System.Serializable]
 public class PieceData
 {
-    public int Id { get; set; };
-    public int Row { get; set; };
+    public int Id { get; set; }
+    public int Row { get; set; }
     public int Column { get; set; }
 
-    public EdgeType TopEdge { get; set; };
-    public EdgeType RightEdge { get; set; };
-    public EdgeType BottomEdge { get; set; };
-    public EdgeType LeftEdge { get; set; };
+    public EdgeType TopEdge { get; set; }
+    public EdgeType RightEdge { get; set; }
+    public EdgeType BottomEdge { get; set; }
+    public EdgeType LeftEdge { get; set; }
 
-    public vector2 Position { get; set; };
-    public float Rotation { get; set; };
+    public Vector2 Position { get; set; }
+    public float Rotation { get; set; }
+
+    public PieceData(int id, int row, int column)
+    {
+        Id = id;
+        Row = row;
+        Column = column;
+
+        TopEdge = EdgeType.Flat;
+        RightEdge = EdgeType.Flat;
+        BottomEdge = EdgeType.Flat;
+        LeftEdge = EdgeType.Flat;
+
+        Position = Vector2.zero;
+        Rotation = 0f;
+
+    }
 }
