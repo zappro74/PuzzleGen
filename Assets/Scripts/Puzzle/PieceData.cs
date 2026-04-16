460e1c304fb343638ba9423ee888900d10e56a6b
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-16 16:32:16 -0500

    init variables

--- Assets/Scripts/Puzzle/PieceData.cs
+++ Assets/Scripts/Puzzle/PieceData.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
+[System.Serializable]
 public class PieceData
 {
-    
+    public int Id { get; set; };
+    public int Row { get; set; };
+    public int Column { get; set; }
+
+    public EdgeType TopEdge { get; set; };
+    public EdgeType RightEdge { get; set; };
+    public EdgeType BottomEdge { get; set; };
+    public EdgeType LeftEdge { get; set; };
+
+    public vector2 Position { get; set; };
+    public float Rotation { get; set; };
 }
