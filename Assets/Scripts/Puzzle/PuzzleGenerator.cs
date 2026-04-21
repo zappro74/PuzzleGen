2f3fbc1c95a6c59e472119f896096bd569732bb3
jdd2121 <jddursch@gmail.com>
2026-04-20 20:01:46 -0500

    Added the PuzzleGenerator class (not complete). Testing to see if I commit/make a pull request properly

---
+++ Assets/Scripts/Puzzle/PuzzleGenerator.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleGenerator
+{
+    public List<PieceData> Generate(int rows, int columns, int generations)
+    {
+        List<PieceData> generatedPieces = new List<PieceData>();
+        return generatedPieces;
+    }
+}
