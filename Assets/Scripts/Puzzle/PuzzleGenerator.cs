2198eaf50ca48ef119024827519529c1f244d8fe
jdd2121 <jddursch@gmail.com>
2026-04-20 20:44:54 -0500

    Mostly finished Issue Implement PuzzleGenerator Fixes #11

--- Assets/Scripts/Puzzle/PuzzleGenerator.cs
+++ Assets/Scripts/Puzzle/PuzzleGenerator.cs
@@ -3,9 +3,28 @@ using UnityEngine;
 
 public class PuzzleGenerator
 {
+
+    //AT THE MOMENT THIS METHOD IS VERY BASIC
+    //ONCE I FINISH THE EDGE ASSIGNMENT, THIS METHOD WILL ALSO SET EACH PIECE'S EDGE DATA
     public List<PieceData> Generate(int rows, int columns, int generations)
     {
-        List<PieceData> generatedPieces = new List<PieceData>();
+        var generatedPieces = new List<PieceData>();
+        var idNumber = 0;
+        var rowTracker = 1;
+        var columnTracker = 1;
+
+        for(var i = 0; i < rows; i++)
+        {
+            for(var j = 0; j < columns; j++)
+            {
+                PieceData piece = new PieceData(idNumber, rowTracker, columnTracker);
+                idNumber++;
+                columnTracker++;
+                generatedPieces.Add(piece);
+
+            }
+            rowTracker++;
+        }
         return generatedPieces;
     }
 }
