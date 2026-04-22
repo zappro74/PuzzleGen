b053567021cbc47c809d6927463251071110d15a
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-22 14:23:40 -0500

    updated CeatePiece method with mesh/collider rendering

--- Assets/Scripts/Rendering/PieceFactory.cs
+++ Assets/Scripts/Rendering/PieceFactory.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class PieceFactory : MonoBehaviour
 {
-    public GameObject CreatePiece(PieceData pieceData)
+
+
+    public GameObject CreatePiece(PieceData pieceData, Material pieceMaterial, float pieceWidth, float pieceHeight, float tabHeight, float edgeMargin, float tabWidth, int pointsPerCurveHalf)
     {
+        List<Vector2> outlinePoints = PieceOutline.BuildPieceOutline(pieceData, pieceWidth, pieceHeight, tabHeight, edgeMargin, tabWidth, pointsPerCurveHalf);
+
         GameObject pieceObject = new GameObject($"Piece_{pieceData.Id}");
+
+        MeshFilter meshFilter = pieceObject.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = pieceObject.AddComponent<MeshRenderer>();
+        PolygonCollider2D polygonCollider = pieceObject.AddComponent<PolygonCollider2D>();
         PuzzlePiece puzzlePiece = pieceObject.AddComponent<PuzzlePiece>();
+
+        Mesh pieceMesh = BuildMesh(outlinePoints); //creating a helper for this shortly
+
+        meshFilter.mesh = pieceMesh;
+        meshRenderer.material = pieceMaterial;
+
+        polygonCollider.points = outlinePoints.ToArray();
 
         puzzlePiece.Initialize(pieceData);
 
         return pieceObject;
-    }
-
-    public void CreateAllPieces(List<PieceData> pieces)
-    {
-        foreach (PieceData pieceData in pieces)
-        {
-            CreatePiece(pieceData);
-        }
     }
 }
