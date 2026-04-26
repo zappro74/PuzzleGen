5c101f1f0b62d7d3b2bc5bd1b5d41bd927b66af9
Zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-26 12:28:13 -0500

    Fixed some formatting and syntax issues

--- Assets/Scripts/Puzzle/ConnectionSystem.cs
+++ Assets/Scripts/Puzzle/ConnectionSystem.cs
@@ -1,4 +1,4 @@
-using Systems.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ConnectionSystem
@@ -15,23 +15,27 @@ public class ConnectionSystem
             PieceBId = bId;
             Direction = direction;
         }
-    }
-    public bool PiecesMatch(int aId, int bId)
-    {
-        return (PieceAId == aId && PieceBId == bId || PieceAId == bId && PieceBId == aId);
-    }
 
-    List<PieceConnection> connections = new List<PieceConnection>();
+        public bool PiecesMatch(int aId, int bId)
+        {
+            return (PieceAId == aId && PieceBId == bId) || (PieceAId == bId && PieceBId == aId);
+        }
+    }
+    
+    private List<PieceConnection> connections = new List<PieceConnection>();
     public bool HasConnection(PieceData a, PieceData b)
     {
-        for(int i = 0; i < connections.Length; i++)
+        for(int i = 0; i < connections.Count; i++)
         {
-            if (PiecesMatch(a.Id, b.Id)) return true;
+            if (connections[i].PiecesMatch(a.Id, b.Id))
+            {
+                return true;
+            }
         }
         return false;
     }
 
-    NeighborDetection neighbors = new NeighborDetection();
+    private NeighborDetection neighbors = new NeighborDetection();
 
     public void AddConnection(PieceData a, PieceData b)
     {
@@ -40,7 +44,15 @@ public class ConnectionSystem
             return;
         }
 
-        if (HasConnection(a, b)) return;
+        if (HasConnection(a, b))
+        {
+            return;
+        }
+
+        if (!neighbors.AreNeighbors(a, b))
+        {
+            return;
+        }
 
         PieceDirection direction = neighbors.GetRelativeDirection(a, b);
 
