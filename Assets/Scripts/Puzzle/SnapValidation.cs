78124f081d43ad9d6462de2c574986e0fafa0805
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-27 19:04:49 -0500

    fixed syntax errors

--- Assets/Scripts/Puzzle/SnapValidation.cs
+++ Assets/Scripts/Puzzle/SnapValidation.cs
@@ -1,54 +1,58 @@
 using UnityEngine;
+
 public class SnapValidation
 {
-    //Snap Tolerance and rotation validation still needs implementation, can include when needed.
-
     public bool CanSnap(PieceData a, PieceData b, float snapTolerance)
     {
-        NeighborDetection neighborDetection = new NeighborDetection;
+        NeighborDetection neighborDetection = new NeighborDetection();
 
-        if(!neighborDetection.AreNeighbors(a,b))
+        if (!neighborDetection.AreNeighbors(a, b))
         {
             return false;
         }
 
         PieceDirection direction = neighborDetection.GetRelativeDirection(a, b);
-        EdgeType edgeA = EdgeType.None;
-        EdgeType edgeB = EdgeType.None;
 
-        switch(direction)
+        EdgeType edgeA;
+        EdgeType edgeB;
+
+        switch (direction)
         {
             case PieceDirection.Right:
                 edgeA = a.RightEdge;
                 edgeB = b.LeftEdge;
                 break;
+
             case PieceDirection.Left:
                 edgeA = a.LeftEdge;
                 edgeB = b.RightEdge;
                 break;
+
             case PieceDirection.Top:
                 edgeA = a.TopEdge;
                 edgeB = b.BottomEdge;
                 break;
+
             case PieceDirection.Bottom:
                 edgeA = a.BottomEdge;
                 edgeB = b.TopEdge;
                 break;
+
             default:
                 return false;
         }
 
-        if(a == EdgeType.Extruded && b == EdgeType.Intruded || a == EdgeType.Intruded && b == EdgeType.Extruded)
+        if ((edgeA == EdgeType.Extruded && edgeB == EdgeType.Intruded) ||
+            (edgeA == EdgeType.Intruded && edgeB == EdgeType.Extruded))
         {
             return true;
         }
-        else if(a == EdgeType.Flat && b == EdgeType.Flat)
+
+        if (edgeA == EdgeType.Flat && edgeB == EdgeType.Flat)
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+
+        return false;
     }
 }
