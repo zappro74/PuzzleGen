4485a2a21cd275512c51d3ddeb424d523c0e7e63
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-29 14:04:16 -0500

    actually set piecewidth/height

--- Assets/Scripts/Managers/GameStateManager.cs
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -44,6 +44,8 @@ public class GameStateManager : MonoBehaviour
     public void GenerateNewPuzzle(Texture2D loadedImage)
     {
         image = loadedImage;
+
+        Vector2 puzzleSize = GetBoardSize(image, boardWidth, boardHeight);
         
         pieceMaterial.mainTexture = loadedImage;
 
@@ -53,6 +55,8 @@ public class GameStateManager : MonoBehaviour
         PieceConfig pieceConfig = new PieceConfig
         {
             pieceMaterial = pieceMaterial,
+            pieceWidth = puzzleSize.x / 3,
+            pieceHeight = puzzleSize.y / 3,
             tabHeight = 0.2f,
             edgeMargin = 0.25f,
             tabWidth = 0.4f,
@@ -67,8 +71,6 @@ public class GameStateManager : MonoBehaviour
             generationSeed = generationSeed,
             pieceConfig = pieceConfig
         };
-
-        Vector2 puzzleSize = GetBoardSize(image, boardWidth, boardHeight);
 
         List<GameObject> pieces = puzzleFactory.GeneratePuzzle(puzzleConfig, puzzleSize.x, puzzleSize.y);
     }
