e894023f6539310940ff0131b2df4ef74da53662
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-04-30 17:19:41 -0500

    added puzzleImage to PuzzleConfig

--- Assets/Scripts/Puzzle/Configs/PuzzleConfig.cs
+++ Assets/Scripts/Puzzle/Configs/PuzzleConfig.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class PuzzleConfig
 {
+    public Texture2D puzzleImage; 
     public int rows;
     public int columns;
     public int generationSeed;
