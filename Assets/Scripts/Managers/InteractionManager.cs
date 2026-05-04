49f9d8ab9e6859347cff63478dff84071be2edd2
Karl Kiefer IV <260264618+user839280@users.noreply.github.com>
2026-05-04 16:43:15 -0500

    Screen Collision & Cleanup

--- Assets/Scripts/Managers/InteractionManager.cs
+++ Assets/Scripts/Managers/InteractionManager.cs
@@ -4,17 +4,11 @@ using UnityEngine.InputSystem;
 
 public class InteractionManager : MonoBehaviour
 {
-    private Camera camera;
     private Transform selection;
     private Vector3 offset;
-    private Renderer renderer;
+    private Renderer render;
     private int order = 1;
-
-    void Start()
-    {
-        camera = Camera.main;
-    }
-
+  
     void Update()
     {
         if (Mouse.current == null) 
@@ -23,26 +17,37 @@ public class InteractionManager : MonoBehaviour
         }
 
         var leftButton = Mouse.current.leftButton;
-        var mousePosition = camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        var mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
         if (leftButton.wasPressedThisFrame)
         {
             // we love linq
-            var topPiece = Physics2D.RaycastAll(mousePosition, Vector2.zero).Where(hit => hit.collider.CompareTag("Piece")).OrderByDescending(hit => hit.transform.GetComponent<Renderer>().sortingOrder).FirstOrDefault();
+            var topPiece = Physics2D.RaycastAll(mousePosition, Vector2.zero).Where(cast => cast.collider.CompareTag("Piece")).OrderByDescending(cast => cast.transform.GetComponent<Renderer>().sortingOrder).FirstOrDefault();
 
             if (topPiece.collider != null)
-            {
+            {   
                 selection = topPiece.transform;
                 offset = selection.position - mousePosition;
-                renderer = topPiece.transform.GetComponent<Renderer>();
-                order = Mathf.Max(order, renderer.sortingOrder) + 1;
-                renderer.sortingOrder = order;
+                render = topPiece.transform.GetComponent<Renderer>();
+                order = Mathf.Max(order, render.sortingOrder) + 1;
+                render.sortingOrder = order;
             }
         }
 
         if (leftButton.isPressed && selection != null)
         {
-            selection.position = mousePosition + offset;
+            var movement = mousePosition + offset;
+            var screenHeight = Camera.main.orthographicSize;
+            var screenWidth = screenHeight * Camera.main.aspect;
+            var cameraPosition = Camera.main.transform.position;
+            var left = cameraPosition.x - screenWidth;
+            var right = cameraPosition.x + screenWidth;
+            var bottom = cameraPosition.y - screenHeight;
+            var top = cameraPosition.y + screenHeight;
+
+            movement.x = Mathf.Clamp(movement.x, left, right - render.bounds.size.x);
+            movement.y = Mathf.Clamp(movement.y, bottom, top - render.bounds.size.y);            
+            selection.position = movement;
         }
 
         if (leftButton.wasReleasedThisFrame)
