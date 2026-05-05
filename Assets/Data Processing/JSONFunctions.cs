c062b13a95b2d3bf516842cb174c719d46589366
karl4kiefer <karl4kiefer@outlook.com>
2026-05-04 20:50:43 -0500

    Things

--- Assets/Data Processing/JSONFunctions.cs
+++ Assets/Data Processing/JSONFunctions.cs
@@ -1,70 +1,70 @@
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Text.Json;
+//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Text.Json;
 
-namespace JSONFunctions
-{
-    public class JSONFileFunctions
-    {
-        [Serializable]
-        public class PieceData
-        {
-            public int Id;
-            public int Row;
-            public int Column;
+//namespace JSONFunctions
+//{
+//    public class JSONFileFunctions
+//    {
+//        [Serializable]
+//        public class PieceData
+//        {
+//            public int Id;
+//            public int Row;
+//            public int Column;
 
-            public EdgeType TopEdge;
-            public EdgeType RightEdge;
-            public EdgeType BottomEdge;
-            public EdgeType LeftEdge;
+//            public EdgeType TopEdge;
+//            public EdgeType RightEdge;
+//            public EdgeType BottomEdge;
+//            public EdgeType LeftEdge;
 
-            public float PositionX;
-            public float PositionY;
-            public float Rotation;
+//            public float PositionX;
+//            public float PositionY;
+//            public float Rotation;
 
-        }
+//        }
 
-        [Serializable]
-        public class PuzzleSaveData
-        {
-            public string imageFilePath;
-            public int rows;
-            public int columns;
-            public int generationSeed;
-            public List<PieceData> pieces;
-        }
+//        [Serializable]
+//        public class PuzzleSaveData
+//        {
+//            public string imageFilePath;
+//            public int rows;
+//            public int columns;
+//            public int generationSeed;
+//            public List<PieceData> pieces;
+//        }
 
 
-        public static void CreateOrEditJSON(PuzzleSaveData data, string filepath)
-        {
-            try
-            {
-                string json = JsonSerializer.Serialize(data, new JsonSerializerOptions
-                {
-                    WriteIndented = true
-                });
+//        public static void CreateOrEditJSON(PuzzleSaveData data, string filepath)
+//        {
+//            try
+//            {
+//                string json = JsonSerializer.Serialize(data, new JsonSerializerOptions
+//                {
+//                    WriteIndented = true
+//                });
 
-                File.WriteAllText(filepath, json);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error in CreateOrEditJSON: " + ex.Message);
-            }
-        }
+//                File.WriteAllText(filepath, json);
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Error in CreateOrEditJSON: " + ex.Message);
+//            }
+//        }
 
-        public static PuzzleSaveData ReadJSON(string filepath)
-        {
-            try
-            {
-                string json = File.ReadAllText(filepath);
-                return JsonSerializer.Deserialize<PuzzleSaveData>(json);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error in ReadJSON:" + ex.Message);
-                return null;
-            }
-        }
-    }
-}
+//        public static PuzzleSaveData ReadJSON(string filepath)
+//        {
+//            try
+//            {
+//                string json = File.ReadAllText(filepath);
+//                return JsonSerializer.Deserialize<PuzzleSaveData>(json);
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Error in ReadJSON:" + ex.Message);
+//                return null;
+//            }
+//        }
+//    }
+//}
