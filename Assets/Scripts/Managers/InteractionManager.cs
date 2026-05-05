319f8b9abfb89d55546d7bb558b933eb08accc2b
karl4kiefer <karl4kiefer@outlook.com>
2026-05-04 22:40:28 -0500

    Added start to snapping in interaction

--- Assets/Scripts/Managers/InteractionManager.cs
+++ Assets/Scripts/Managers/InteractionManager.cs
@@ -1,9 +1,11 @@
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InteractionManager : MonoBehaviour
 {
+    [Header("Script Connections")]
+    public SnappingManager snapManager;
+
     private Camera gameCamera;
     private Transform selection;
     private Vector3 offset;
@@ -16,7 +18,7 @@ public class InteractionManager : MonoBehaviour
     }
     void Update()
     {
-        if (Mouse.current == null) 
+        if (Mouse.current == null || gameCamera == null)
         {
             return;
         }
@@ -26,38 +28,86 @@ public class InteractionManager : MonoBehaviour
 
         if (leftButton.wasPressedThisFrame)
         {
-            // we love linq
-            var topPiece = Physics2D.RaycastAll(mousePosition, Vector2.zero).Where(cast => cast.collider.CompareTag("Piece")).OrderByDescending(cast => cast.transform.GetComponent<Renderer>().sortingOrder).FirstOrDefault();
-
-            if (topPiece.collider != null)
-            {   
-                selection = topPiece.transform;
-                offset = selection.position - mousePosition;
-                render = topPiece.transform.GetComponent<Renderer>();
-                order = Mathf.Max(order, render.sortingOrder) + 1;
-                render.sortingOrder = order;
-            }
+            TryPickup(mousePosition);
         }
-
-        if (leftButton.isPressed && selection != null)
+        if (leftButton.isPressed && selection != null && render != null)
         {
             var movement = mousePosition + offset;
-            var screenHeight = gameCamera.orthographicSize;
-            var screenWidth = screenHeight * gameCamera.aspect;
-            var cameraPosition = gameCamera.transform.position;
-            var left = cameraPosition.x - screenWidth;
-            var right = cameraPosition.x + screenWidth;
-            var bottom = cameraPosition.y - screenHeight;
-            var top = cameraPosition.y + screenHeight;
-
-            movement.x = Mathf.Clamp(movement.x, left, right - render.bounds.size.x);
-            movement.y = Mathf.Clamp(movement.y, bottom, top - render.bounds.size.y);            
-            selection.position = movement;
+            selection.position = ScreenBoundaries(movement);
         }
-
         if (leftButton.wasReleasedThisFrame)
         {
+            if (selection != null && snapManager != null)
+            {
+                snapManager.TrySnap(selection);
+            }
             selection = null;
         }
+    }
+    private RaycastHit2D GrabPiece(Vector3 mousePosition)
+    {
+        var hitPieces = Physics2D.RaycastAll(mousePosition, Vector2.zero);
+        int highestOrder = int.MinValue;
+        var topPiece = new RaycastHit2D();
+
+        foreach (var piece in hitPieces)
+        {
+            if (piece.collider != null && piece.collider.CompareTag("Piece"))
+            {
+                var render = piece.transform.GetComponent<Renderer>();
+
+                if (render != null && render.sortingOrder > highestOrder)
+                {
+                    highestOrder = render.sortingOrder;
+                    topPiece = piece;
+                }
+            }
+        }
+        return topPiece;
+    }
+    private void TryPickup(Vector3 mousePosition)
+    {
+        var topPiece = GrabPiece(mousePosition);
+
+        if (topPiece.collider != null)
+        {
+            selection = GetRoot(topPiece.transform);
+            offset = selection.position - mousePosition;
+            order++;
+
+            var renderers = selection.GetComponentsInChildren<Renderer>();
+
+            foreach (var renderer in renderers)
+            {
+                renderer.sortingOrder = order;
+            }
+
+            render = topPiece.transform.GetComponent<Renderer>();
+
+        }
+    }
+    private Vector3 ScreenBoundaries(Vector3 movement)
+    {
+        var screenHeight = gameCamera.orthographicSize;
+        var screenWidth = screenHeight * gameCamera.aspect;
+        var cameraPosition = gameCamera.transform.position;
+        var left = cameraPosition.x - screenWidth;
+        var right = cameraPosition.x + screenWidth;
+        var bottom = cameraPosition.y - screenHeight;
+        var top = cameraPosition.y + screenHeight;
+
+        movement.x = Mathf.Clamp(movement.x, left, right - render.bounds.size.x);
+        movement.y = Mathf.Clamp(movement.y, bottom, top - render.bounds.size.y);
+
+        return movement;
+    }
+    private Transform GetRoot(Transform piece)
+    {
+        while (piece.parent != null && piece.parent.GetComponent<PuzzlePiece>() != null)
+        {
+            piece = piece.parent;
+        }
+
+        return piece;
     }
 }
